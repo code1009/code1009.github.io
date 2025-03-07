723545ff60b3682d812736506a15aff135d466d7
code1009 <code1009@naver.com>
2025-03-08 02:47:04 +0900

    `

--- readme.cpp
+++ readme.cpp
@@ -1,3 +1,5 @@
 ﻿code1009.github.io
 
 https://github.com/CaiJimmy/hugo-theme-stack-starter
+https://demo.stack.jimmycai.com/p/markdown-syntax-guide/
+https://github.com/CaiJimmy/hugo-theme-stack-starter/blob/master/content/post/markdown-syntax/index.md?plain=1
