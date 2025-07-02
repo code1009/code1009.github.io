17dedfd16371a73fd9340ba5429ab1467030a924
code1009 <code1009@naver.com>
2025-07-02 18:24:55 +0900

    1

--- web/mainmenu.js
+++ web/mainmenu.js
@@ -20,6 +20,7 @@ class MainMenu {
 	registerMenuDataItem() {
 		//this._Menu.addDataItem("/예시" , "../0000.example/page.html");
 		//this._Menu.addDataItem("/틀"   , "../0000.template/page.html");
+
 		this._Menu.addDataItem("/셈말짓기"                                     , "../셈말짓기/page.html");
 		this._Menu.addDataItem("/윈도우개발/윈도우 API utf8 설정"              , "../윈도우 API utf8 설정/page.html");      
 		this._Menu.addDataItem("/윈도우개발/WinUI3 vcxprj 설정"                , "../WinUI3 vcxprj 설정/page.html");
@@ -29,7 +30,7 @@ class MainMenu {
 		this._Menu.addDataItem("/윈도우개발/Visual Studio Plugin"              , "../Visual Studio Plugin/page.html");         
 		this._Menu.addDataItem("/윈도우개발/Visual Studio C++ 빌드설정"        , "../Visual Studio C++ 빌드설정/page.html");
 		this._Menu.addDataItem("/윈도우개발/Visual Studio 기본설정"            , "../Visual Studio 기본설정/page.html");
-		this._Menu.addDataItem("/개발/git"            , "../git/page.html");
+		this._Menu.addDataItem("/개발/git"                                     , "../git/page.html");
 	}
 	
 	initializeMenu() {
