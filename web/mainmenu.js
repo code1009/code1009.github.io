5ebfc1c92f89ffc32ce99899505073d765baf0a2
code1009 <code1009@naver.com>
2025-07-07 09:35:17 +0900

    1

--- web/mainmenu.js
+++ web/mainmenu.js
@@ -22,17 +22,17 @@ class MainMenu {
 		//this._Menu.addDataItem("/틀"   , "../template/page.html");
 		//this._Menu.addDataItem("/틀"   , "../template-markdown/page.html");
 
-		this._Menu.addDataItem("/셈말짓기"                                     , "../page.셈말짓기/page.html");
-		this._Menu.addDataItem("/윈도우개발/윈도우 API utf8 설정"              , "../page.윈도우 API utf8 설정/page.html");      
-		this._Menu.addDataItem("/윈도우개발/WinUI3 vcxprj 설정"                , "../page.WinUI3 vcxprj 설정/page.html");
-		this._Menu.addDataItem("/윈도우개발/vld"                               , "../page.vld/page.html");
-		this._Menu.addDataItem("/윈도우개발/vcpkg"                             , "../page.vcpkg/page.html");           
-		this._Menu.addDataItem("/윈도우개발/nuget"                             , "../page.nuget/page.html");         
-		this._Menu.addDataItem("/윈도우개발/Visual Studio Plugin"              , "../page.Visual Studio Plugin/page.html");         
-		this._Menu.addDataItem("/윈도우개발/Visual Studio C++ 빌드설정"        , "../page.Visual Studio C++ 빌드설정/page.html");
-		this._Menu.addDataItem("/윈도우개발/Visual Studio 기본설정"            , "../page.Visual Studio 기본설정/page.html");
-		this._Menu.addDataItem("/개발/git"                                     , "../page.git/page.html");
-		this._Menu.addDataItem("/개발/markdown"                                , "../page.markdown/page.html");
+		this._Menu.addDataItem("/셈말짓기"                                     , "../page-셈말짓기/page.html");
+		this._Menu.addDataItem("/윈도우개발/윈도우 API utf8 설정"              , "../page-윈도우 API utf8 설정/page.html");      
+		this._Menu.addDataItem("/윈도우개발/WinUI3 vcxprj 설정"                , "../page-WinUI3 vcxprj 설정/page.html");
+		this._Menu.addDataItem("/윈도우개발/vld"                               , "../page-vld/page.html");
+		this._Menu.addDataItem("/윈도우개발/vcpkg"                             , "../page-vcpkg/page.html");           
+		this._Menu.addDataItem("/윈도우개발/nuget"                             , "../page-nuget/page.html");         
+		this._Menu.addDataItem("/윈도우개발/Visual Studio Plugin"              , "../page-Visual Studio Plugin/page.html");         
+		this._Menu.addDataItem("/윈도우개발/Visual Studio C++ 빌드설정"        , "../page-Visual Studio C++ 빌드설정/page.html");
+		this._Menu.addDataItem("/윈도우개발/Visual Studio 기본설정"            , "../page-Visual Studio 기본설정/page.html");
+		this._Menu.addDataItem("/개발/git"                                     , "../page-git/page.html");
+		this._Menu.addDataItem("/개발/markdown"                                , "../page-markdown/page.html");
 	}
 	
 	initializeMenu() {
