3eb830dc01f66da2b1030895575278b79403db7d
code1009 <code1009@naver.com>
2025-07-07 15:38:58 +0900

    1

--- web/markdown.js
+++ web/markdown.js
@@ -10,8 +10,9 @@
 //===========================================================================
 function loadMarkDownScript(callback) {
 	var script = document.createElement('script');
-	script.src = "https://cdn.jsdelivr.net/npm/showdown/dist/showdown.min.js";
+	//script.src = "https://cdn.jsdelivr.net/npm/showdown/dist/showdown.min.js";
 	//script.src = "https://cdn.jsdelivr.net/npm/marked/marked.min.js";
+	script.src = https://cdn.jsdelivr.net/npm/marked/lib/marked.umd.js;
 	script.type = "text/javascript";
 	script.onload = callback; 
 	document.head.appendChild(script);
@@ -25,11 +26,11 @@ function renderMarkdwon(markdown_view_id, markdown_url) {
 				const markdown = this.responseText;
 
 				//marked
-				//const html = marked.parse(markdown);
+				const html = marked.parse(markdown);
 
 				//showdown
-				const converter = new showdown.Converter();
-				const html = converter.makeHtml(markdown);
+				//const converter = new showdown.Converter();
+				//const html = converter.makeHtml(markdown);
 
 				document.getElementById(markdown_view_id).innerHTML = html;
 			}
