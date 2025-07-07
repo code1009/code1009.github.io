481fc88682eab8d1f63f343982fb3a5ca1f2c76b
code1009 <code1009@naver.com>
2025-07-07 16:32:46 +0900

    1

--- web/page-git/submenu.js
+++ web/page-git/submenu.js
@@ -19,9 +19,9 @@ class SubMenu {
 
 	registerMenuDataItem() {
 		this._Menu.addDataItem("/git"                    , "./page.html?page=page1");
-		this._Menu.addDataItem("/git/자주사용하는 명령어", "./page1.html");
-		this._Menu.addDataItem("/git/rebase", "./page2.html");
-		this._Menu.addDataItem("/git/branch 이름 규칙", "./page3.html");
+		this._Menu.addDataItem("/git/자주사용하는 명령어", "./page.html?page=page1");
+		this._Menu.addDataItem("/git/rebase"             , "./page.html?page=page2");
+		this._Menu.addDataItem("/git/branch 이름 규칙"   , "./page.html?page=page3");
 
 		this._Menu.getRootItem().setExpanded(true);
 		this._Menu.makeTreeItems();
