9a9a27b930e2dca12c403e3acdfae33bbcb003ea
code1009 <code1009@naver.com>
2025-07-08 15:18:23 +0900

    1

--- web/mainmenu.js
+++ web/mainmenu.js
@@ -21,7 +21,8 @@ class MainMenu {
 		//this._Menu.addDataItem("/예시" , "../example/page.html");
 		//this._Menu.addDataItem("/틀"   , "../template/page.html");
 
-		this._Menu.addDataItem("/셈말짓기"                     , "../home-셈말짓기/page.html");
+		this._Menu.addDataItem("/셈말짓기"                      , "../home-셈말짓기/page.html");
+		this._Menu.addDataItem("/개발 노트/github"              , "../page-github/page.html");
 		this._Menu.addDataItem("/개발 노트/git"                 , "../page-git/page.html");
 		this._Menu.addDataItem("/개발 노트/markdown"            , "../page-markdown/page.html");
 		this._Menu.addDataItem("/개발 노트/오픈소스 라이브러리" , "../page-open-source-lib/page.html");
---
+++ web/page-github/submenu.js
@@ -0,0 +1,58 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+"use strict";
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+class SubMenu {
+
+	_Menu = new Menu();
+
+	constructor() {
+		this.registerMenuDataItem();
+		this.initializeMenu();
+	}
+
+	registerMenuDataItem() {
+		this._Menu.addDataItem("/github"            , "./page.html?page=page1");
+		this._Menu.addDataItem("/github/이슈 템플릿", "./page.html?page=page1");
+
+		this._Menu.getRootItem().setExpanded(true);
+		this._Menu.makeTreeItems();
+		this._Menu.render("subMenu");
+	}
+
+	initializeMenu() {
+		this._Menu.makeTreeItems();
+
+		this._Menu.getRootItem().setExpanded(true);
+
+		for (let child of this._Menu.getRootItem().getChildren()) {
+			child.setExpanded(true);
+		}
+
+		this._Menu.render("subMenu");
+	}
+}
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+var _SubMenu = null;
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+function initializeSubMenu() {
+	_SubMenu = new SubMenu();
+}
