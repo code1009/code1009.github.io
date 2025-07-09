322a37e19ed000d096efda1b0d334f3bd9d5e189
code1009 <code1009@naver.com>
2025-07-09 11:30:21 +0900

    1

--- web/mainmenu.js
+++ web/mainmenu.js
@@ -19,9 +19,13 @@ class MainMenu {
 	
 	registerMenuDataItem() {
 		//this._Menu.addDataItem("/예시" , "../example/page.html");
-		//this._Menu.addDataItem("/틀"   , "../template/page.html");
 
-		this._Menu.addDataItem("/셈말짓기"                      , "../home-셈말짓기/page.html");
+		//this._Menu.addDataItem("/틀-단일"   , "../template-m/page.html");
+		//this._Menu.addDataItem("/틀-다중"   , "../template-s/page.html");
+
+		this._Menu.addDataItem("/셈말짓기", "../home-셈말짓기/page.html");
+		
+		this._Menu.addDataItem("/개발 노트"                     , "../page-github/page.html");
 		this._Menu.addDataItem("/개발 노트/github"              , "../page-github/page.html");
 		this._Menu.addDataItem("/개발 노트/git"                 , "../page-git/page.html");
 		this._Menu.addDataItem("/개발 노트/markdown"            , "../page-markdown/page.html");
@@ -29,6 +33,7 @@ class MainMenu {
 		this._Menu.addDataItem("/개발 노트/Visual Studio"       , "../page-vs/page.html");
 		this._Menu.addDataItem("/개발 노트/Code Composer Studio", "../page-ccs/page.html");
 
+		this._Menu.addDataItem("/윈도우즈 프로그래밍",        "../page-WinUI3/page.html");
 		this._Menu.addDataItem("/윈도우즈 프로그래밍/WinUI3", "../page-WinUI3/page.html");
 	}
 	
