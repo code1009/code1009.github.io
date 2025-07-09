af67b3816332dec10482f469b10bd64f63f54b31
code1009 <code1009@naver.com>
2025-07-09 11:25:37 +0900

    1

---
+++ web/template-m/submenu.js
@@ -0,0 +1,59 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+"use strict";
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+class SubMenu {
+
+	_Menu = new Menu();
+
+	constructor() {
+		this.registerMenuDataItem();
+		this.initializeMenu();
+	}
+
+	registerMenuDataItem() {
+		this._Menu.addDataItem("/분류"         , "./page.html?page=page1");
+		this._Menu.addDataItem("/분류/제목1"   , "./page.html?page=page1");
+		this._Menu.addDataItem("/분류/제목2"   , "./page.html?page=page2");
+
+		this._Menu.getRootItem().setExpanded(true);
+		this._Menu.makeTreeItems();
+		this._Menu.render("subMenu");
+	}
+
+	initializeMenu() {
+		this._Menu.makeTreeItems();
+
+		this._Menu.getRootItem().setExpanded(true);
+
+		for (let child of this._Menu.getRootItem().getChildren()) {
+			child.setExpanded(true);
+		}
+
+		this._Menu.render("subMenu");
+	}
+}
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+var _SubMenu = null;
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+function initializeSubMenu() {
+	_SubMenu = new SubMenu();
+}
