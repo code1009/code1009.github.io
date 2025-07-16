2a3876debaee89a2aa04b5054704a70ca11b1117
code1009 <code1009@naver.com>
2025-07-16 16:26:47 +0900

    1

--- web/markdown.js
+++ web/markdown.js
@@ -11,13 +11,37 @@
 // https://github.com/markedjs/marked
 //===========================================================================
 
-function loadMarkDownScript(callback) {
+function load_marked_script(callback) {
 	var script = document.createElement('script');
 	//script.src = "https://cdn.jsdelivr.net/npm/marked/marked.min.js";
 	script.src = "https://cdn.jsdelivr.net/npm/marked/lib/marked.umd.js";
 	script.type = "text/javascript";
-	script.onload = callback; 
+	script.onload = callback;
 	document.head.appendChild(script);
+}
+
+function load_highlight_script(callback) {
+	var script = document.createElement('script');
+	script.src = "https://cdn.jsdelivr.net/npm/highlight.js/lib/highlight.js";
+	script.type = "text/javascript";
+	script.onload = callback;
+	document.head.appendChild(script);
+}
+
+function load_highlight_link_stylesheet() {
+	var link = document.createElement('link');
+	link.href = "https://cdn.jsdelivr.net/npm/highlight.js/styles/github.css";
+	link.rel  = "stylesheet";
+	document.head.appendChild(link);
+}
+
+function loadMarkDownScript(callback) {
+	load_marked_script(
+		function () {
+			load_highlight_stylesheet();
+			load_highlight_script(callback);
+		}
+	);
 }
 
 function renderMarkdwon(markdownViewId, markdownFileURL) {
